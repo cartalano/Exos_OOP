29e73ec6a62a35b0be84fe29c85ed3dc0cdd327a
Nicolas Catalano <nicolascatalano1@gmail.com>
2024-09-04 16:59:29 +0200

    premier test php oop

---
+++ Exos_OOP/index.php
@@ -0,0 +1,44 @@
+<?php
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
+
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function set_color($color) {
+    $this->color = $color;
+  }
+  function get_color() {
+    return $this->color;
+  }
+}
+
+$apple = new Fruit();
+$apple->set_name('Apple');
+$apple->set_color('Red');
+echo "Name: " . $apple->get_name();
+echo "<br>";
+echo "Color: " . $apple->get_color();
+
+class Car {
+  public $name;
+  public $color;
+
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  function __destruct() {
+    echo "The car is {$this->name} and the color is {$this->color}.";
+  }
+}
+
+$porsche = new Car("Porsche", "black");
+
+?>
